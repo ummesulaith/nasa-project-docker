73aac97bfa7313bb6119aca051a5dd19daa36b56
Umme Suleyath <ummesul76@gmail.com>
2023-03-07 18:31:52 +0530

    Refactor: seperated the mongo db connection

--- server/src/server.js
+++ server/src/server.js
@@ -11,24 +11,16 @@
 
 
 const http = require('http');
-const mongoose = require('mongoose');
 const app = require('./app');
 
 const {loadPlanetsData} = require('./models/planets.model')
 const PORT = process.env.PORT || 8000;
-const MONGO_URL = 'mongodb+srv://nasa-api:OYjp9XPtWGOV7VkX@nasacluster.6tozxe7.mongodb.net/?retryWrites=true&w=majority'
-
+const {mongoConnect} = require('./services/mongo')
 const server = http.createServer(app);
 
-mongoose.connection.once('open',()=>{
-  console.log('MongoDb is ready')
-})
-mongoose.connection.on('error',(err)=>{
-  console.log('MongoDb is not ready', err)
-})
 
 async function startServer(){
-  await mongoose.connect(MONGO_URL)
+  await mongoConnect()
     await loadPlanetsData()
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);

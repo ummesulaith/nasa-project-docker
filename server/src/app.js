c1e049103f083ef073b4b64554e7ddcc6ad8bece
Umme Suleyath <ummesul76@gmail.com>
2023-03-08 19:41:26 +0530

    Refactor: added versioning

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', 'public')))
-app.use('v1',api)
+app.use('/v1', api);
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'..','public','index.html'))
 })

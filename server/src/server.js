17cb24d6ff3826425162cb917e87d886939302cc
Umme Suleyath <ummesul76@gmail.com>
2023-03-23 22:30:39 +0530

    Refactor: added dotenv

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,8 @@
 
 const http = require('http');
 const app = require('./app');
-
+const {loadLaunchData} = require('./models/launches.model')
+require ('dotenv').config()
 const {loadPlanetsData} = require('./models/planets.model')
 const PORT = process.env.PORT || 8000;
 const {mongoConnect} = require('./services/mongo')
@@ -22,6 +23,7 @@ const server = http.createServer(app);
 async function startServer(){
   await mongoConnect()
     await loadPlanetsData()
+    await loadLaunchData()
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
